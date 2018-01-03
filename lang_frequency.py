7f0bca776f9be56ee6577de843358f6474c0c608
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2018-01-03 10:24:07 +0300

    changed argument name n to word_num

--- lang_frequency.py
+++ lang_frequency.py
@@ -6,7 +6,7 @@ import argparse
 def parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file')
-    parser.add_argument('-n', type=int)
+    parser.add_argument('--word_num', type=int)
     return parser.parse_args()
 
 
@@ -19,14 +19,14 @@ def get_words(filepath):
     return re.findall('(?=\D)\w+', load_data(filepath))
 
 
-def get_most_frequent_words(filepath, n):
-    return Counter(get_words(filepath)).most_common(n)
+def get_most_frequent_words(filepath, word_num):
+    return Counter(get_words(filepath)).most_common(word_num)
 
 
 if __name__ == '__main__':
     filepath = parser().file
-    n = parser().n
+    word_num = parser().word_num
     print('Most common words are (word, times):')
-    print(*get_most_frequent_words(filepath, n), sep='\n')
+    print(*get_most_frequent_words(filepath, word_num), sep='\n')
 
 
